fb5ce9b0cd3c73962f0d8a25d85201849d96b36f
kanggaro <90663373+kanggaro@users.noreply.github.com>
2021-12-05 14:01:54 +0700

    add `credits` and `gameOver`

---
+++ FP-PBO/src/its/fp/GameOver.java
@@ -0,0 +1,14 @@
+package its.fp;
+
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+
+public class GameOver extends JFrame {
+
+	
+	public GameOver() {
+		super("GameOver");
+		setLayout(new FlowLayout());
+		
+	}
+}
