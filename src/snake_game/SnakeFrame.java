d2c0031063f9f288996b98a178f39a77c1066d37
AfifArdhiansyah <afifardhiansyah@gmail.com>
2021-12-08 18:01:56 +0700

    Dhiwa & Afif: Game arena

---
+++ src/snake_game/SnakeFrame.java
@@ -0,0 +1,26 @@
+package snake_game;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.*;
+
+public class SnakeFrame extends JFrame  {
+    
+    public SnakeFrame() {
+        
+        add(new Snake());
+        setResizable(false);
+        pack();
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException {
+      JFrame jf = new SnakeFrame();
+      jf.setJMenuBar(Snake.mb);
+      jf.setVisible(true);
+    }
+}
